654fd651caf32e0c0349941e1c2ca3155b7568dd
Daniel <d.alejandro1203@gmail.com>
2026-02-02 20:49:17 -0400

    Initial Commit

---
+++ job_application/forms.py
@@ -0,0 +1,12 @@
+from django import forms
+
+
+class applicationform(forms.Form):
+        first_name = forms.CharField(max_length=80)
+        last_name = forms.CharField(max_length=80)
+        email = forms.EmailField()
+        date = forms.DateField()
+        occupation = forms.CharField(max_length=80)
+
+
+
